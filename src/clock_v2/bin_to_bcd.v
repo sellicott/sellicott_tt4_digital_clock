29bc05d7d7d3708be1181513ce522553d22cfd2d
Sam Ellicott <sellicott@cedarville.edu>
2023-11-03 07:10:28 -0400

    testbench for converting binary to bcd values

--- src/clock_v2/bin_to_bcd.v
+++ src/clock_v2/bin_to_bcd.v
@@ -13,54 +13,57 @@ module bin_to_bcd (
     i_bin,
     o_bcd_lsb,
     o_bcd_msb
-)
+);
 
 /* verilator lint_off UNUSED */
-// clock pin for the testbench
 input  wire       i_clk;
-/* verilator lint_on UNUSED */
+/* verilator lint_on  UNUSED */
 input  wire [5:0] i_bin;
 output wire [3:0] o_bcd_lsb;
 output wire [3:0] o_bcd_msb;
 
 // our input is in the range [0, 63] meaning, we have 7 possible outputs for
 // the msb
-wire msb_0 = (i_bin >=0)   && (i_bin < 10);
-wire msb_1 = (i_bin >= 10) && (i_bin < 20);
-wire msb_2 = (i_bin >= 20) && (i_bin < 30);
-wire msb_3 = (i_bin >= 30) && (i_bin < 40);
-wire msb_4 = (i_bin >= 40) && (i_bin < 50);
-wire msb_5 = (i_bin >= 50) && (i_bin < 60);
-wire msb_6 = (i_bin >= 60) && (i_bin <= 63);
+wire msb_0 =                     (i_bin < 6'd10);
+wire msb_1 = (i_bin >= 6'd10) && (i_bin < 6'd20);
+wire msb_2 = (i_bin >= 6'd20) && (i_bin < 6'd30);
+wire msb_3 = (i_bin >= 6'd30) && (i_bin < 6'd40);
+wire msb_4 = (i_bin >= 6'd40) && (i_bin < 6'd50);
+wire msb_5 = (i_bin >= 6'd50) && (i_bin < 6'd60);
+/* verilator lint_off CMPCONST */
+wire msb_6 = (i_bin >= 6'd60) && (i_bin <= 6'd63);
+/* verilator lint_on CMPCONST */
 
 wire [6:0] msb_one_hot = {msb_6, msb_5, msb_4, msb_3, msb_2, msb_1, msb_0};
 reg [3:0] bcd_msb = 0;
+/* verilator lint_off UNUSEDSIGNAL */
 reg [5:0] bcd_lsb = 0;
+/* verilator lint_on UNUSEDSIGNAL */
 
 
 always @(*) begin
    case(msb_one_hot)
-       7'h40: bcd_msb = 6;
-       7'h20: bcd_msb = 5;
-       7'h10: bcd_msb = 4;
-       7'h08: bcd_msb = 3;
-       7'h04: bcd_msb = 2;
-       7'h02: bcd_msb = 1;
-       7'h01: bcd_msb = 0;
+       7'h40: bcd_msb   = 4'd6;
+       7'h20: bcd_msb   = 4'd5;
+       7'h10: bcd_msb   = 4'd4;
+       7'h08: bcd_msb   = 4'd3;
+       7'h04: bcd_msb   = 4'd2;
+       7'h02: bcd_msb   = 4'd1;
+       7'h01: bcd_msb   = 4'd0;
        default: bcd_msb = 4'hF;
    endcase
 end
 
 always @(*) begin
    case(bcd_msb)
-       4'h6: bcd_lsb = i_bin - 60;
-       4'h5: bcd_lsb = i_bin - 50;
-       4'h4: bcd_lsb = i_bin - 40;
-       4'h3: bcd_lsb = i_bin - 30;
-       4'h2: bcd_lsb = i_bin - 20;
-       4'h1: bcd_lsb = i_bin - 10;
-       4'h0: bcd_lsb = i_bin - 00;
-       default: bcd_lsb = 4'hF;
+       4'h6: bcd_lsb    = i_bin - 6'd60;
+       4'h5: bcd_lsb    = i_bin - 6'd50;
+       4'h4: bcd_lsb    = i_bin - 6'd40;
+       4'h3: bcd_lsb    = i_bin - 6'd30;
+       4'h2: bcd_lsb    = i_bin - 6'd20;
+       4'h1: bcd_lsb    = i_bin - 6'd10;
+       4'h0: bcd_lsb    = i_bin - 6'd00;
+       default: bcd_lsb = 6'h0;
    endcase
 end
 
--- src/verilator_testbenches/bin_to_bcd.cpp
+++ src/verilator_testbenches/bin_to_bcd.cpp
@@ -9,14 +9,29 @@ int main(int nargs, char **args) {
 
   for (int i = 0; i < 64; ++i) {
     tb->core->i_bin = i;
+    tb->tick();
     int msb = tb->core->o_bcd_msb;
     int lsb = tb->core->o_bcd_lsb;
-
-    tb->tick();
+    printf("0x%02x -> %d%d\n", i, msb, lsb);
     assert(lsb == i % 10);
     assert(msb == i / 10);
-    printf("0x%04x -> %d%d\n", i, msb, lsb);
   }
+
+  tb->core->i_bin = 64;
+  tb->tick();
+  int msb = tb->core->o_bcd_msb;
+  int lsb = tb->core->o_bcd_lsb;
+  printf("0x%02x -> %d%d\n", 64, msb, lsb);
+  // assert(lsb == 0);
+  // assert(msb == 0);
+
+  tb->core->i_bin = 65;
+  tb->tick();
+  msb = tb->core->o_bcd_msb;
+  lsb = tb->core->o_bcd_lsb;
+  printf("0x%02x -> %d%d\n", 65, msb, lsb);
+  assert(lsb == 1);
+  assert(msb == 0);
 
   delete tb;
   return 0;
